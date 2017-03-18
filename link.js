ac72b33984cfe5db952db1517566f479a0a93db4
zhaoxi06 <731856653@qq.com>
2017-03-18 09:33:07 +0800

    accomplish

---
+++ link.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+window.onload = function ()
+{
+	//头部信息
+	var oTop = document.getElementById('top');
+	var oEmail = document.getElementById('email');
+	var adivEmail = oEmail.getElementsByTagName('div');
+	var oFriendLinks = document.getElementById('friendLinks');
+	var oMenu = document.getElementById('menu');	
+	//图片新闻
+	var oPic = document.getElementById('pic');
+	//具体内容
+	var oContent = document.getElementById('content');
+	
+	oEmail.onmouseover = function (){
+		adivEmail[0].style.display = block;
+	};
+
+
+	var oTop = document.getElementById('top');
+	var oEmail = document.getElementById('email');
+	var oFriendLinks = document.getElementById('friendLinks');
+	var oMenu = document.getElementById('menu');	
+	var oPic = document.getElementById('pic');
+	var oContent = document.getElementById('content');
+	
+	oEmail.onmouseover = function ()
+	{
+		
+	};
+};
